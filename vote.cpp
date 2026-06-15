47fe9315174fbfbe56ce39161175615ebf9a5470
mohdtalib0046-lang <mohdtalib0046@gmail.com>
2026-06-15 12:29:49 +0530

    first commit

---
+++ vote.cpp
@@ -0,0 +1,20 @@
+// # include <iostream>
+// using namespace std;
+// int main (){
+//     int age;
+//     cout<<"enter your age ";cin>>age;
+//     if(age>=18) {
+//         cout<<"you can drive";
+//         return 0;
+//     }
+//     cout<<"you can not drive";
+//     return 0;
+// }
+
+# include <iostream>
+using namespace std;
+int main(){
+       return 0;
+        
+}
+
